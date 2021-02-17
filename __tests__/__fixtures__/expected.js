b75639b2db3017ce68a77ff488951ac12ee976f7
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-02-17 20:58:43 +0700

    step 2

---
+++ __tests__/__fixtures__/expected.js
@@ -0,0 +1,24 @@
+export default {
+  feed: { title: 'Новые уроки на Хекслете', description: 'Практические уроки по программированию', id: '1' },
+  posts: [{
+    title: 'Переполнение / Основы вёрстки контента', description: 'Цель: Изучить работу свойства overflow и способах скрытия контента внутри блоков. Узнать о свойстве text-overflow', link: 'https://ru.hexlet.io/courses/css-content/lessons/overflow/theory_unit', postId: '1_0', feedId: '1',
+  }, {
+    title: 'Формы / Основы вёрстки контента', description: 'Цель: Изучить создание одного из самых важных элементов на сайте — формы. Научиться использовать стандартные элементы ввода данных и отправки формы', link: 'https://ru.hexlet.io/courses/css-content/lessons/forms/theory_unit', postId: '1_1', feedId: '1',
+  }, {
+    title: 'Псевдоэлементы / Основы вёрстки контента', description: 'Цель: Познакомиться с псевдоэлементами и научиться генерировать новый контент через CSS', link: 'https://ru.hexlet.io/courses/css-content/lessons/pseudoelements/theory_unit', postId: '1_2', feedId: '1',
+  }, {
+    title: 'Псевдоклассы / Основы вёрстки контента', description: 'Цель: Узнать о возможностях стилизации событий элемента и стилизации по месторасположению элемента. Научиться использовать псевдоклассы', link: 'https://ru.hexlet.io/courses/css-content/lessons/pseudoclasses/theory_unit', postId: '1_3', feedId: '1',
+  }, {
+    title: 'Селекторы / Основы вёрстки контента', description: 'Цель: Изучить более сложные селекторы второго и третьего уровня по спецификации W3C', link: 'https://ru.hexlet.io/courses/css-content/lessons/selectors/theory_unit', postId: '1_4', feedId: '1',
+  }, {
+    title: 'Таблицы / Основы вёрстки контента', description: 'Цель: Научиться верстать таблицы. Узнать о семантике таблиц', link: 'https://ru.hexlet.io/courses/css-content/lessons/table/theory_unit', postId: '1_5', feedId: '1',
+  }, {
+    title: 'Медиаэлементы / Основы вёрстки контента', description: 'Цель: Узнать о способах добавления медиаэлементов на страницу Изучить возможности при вставке изображений, видео, аудио', link: 'https://ru.hexlet.io/courses/css-content/lessons/media/theory_unit', postId: '1_6', feedId: '1',
+  }, {
+    title: 'Единицы измерения / Основы вёрстки контента', description: 'Цель: Изучить единицы измерения, доступные в CSS, и их взаимосвязи между собой', link: 'https://ru.hexlet.io/courses/css-content/lessons/units/theory_unit', postId: '1_7', feedId: '1',
+  }, {
+    title: 'Колонки / Основы вёрстки контента', description: 'Цель: Познакомиться с CSS Multi-column Layout и узнать о способах его применения', link: 'https://ru.hexlet.io/courses/css-content/lessons/columns/theory_unit', postId: '1_8', feedId: '1',
+  }, {
+    title: 'Объекты-Сущности, Объекты-Значения и внедренные объекты / PHP: Объектно-ориентированный дизайн', description: 'Цель: Познакомиться с основными принципами организации объектов описывающих предметную область', link: 'https://ru.hexlet.io/courses/php-object-oriented-design/lessons/modeling/theory_unit', postId: '1_9', feedId: '1',
+  }],
+};
--- __tests__/test.js
+++ __tests__/test.js
@@ -2,13 +2,16 @@
 
 import { promises as fs } from 'fs';
 import path from 'path';
+import rssParser from '../src/rss-parser';
+import expectedParsedData from './__fixtures__/expected';
+
+let rssData;
 
 beforeEach(async () => {
-  const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
-  const html = await fs.readFile(pathToHtml, 'utf8');
-  document.body.innerHTML = html;
+  const pathToRss = path.resolve(__dirname, '__fixtures__/rss-feed.xml');
+  rssData = await fs.readFile(pathToRss, 'utf8');
 });
 
-test('init', () => {
-  expect(true).toBeDefined();
+test('rssParser test', () => {
+  expect(rssParser(rssData)).toEqual(expectedParsedData);
 });
