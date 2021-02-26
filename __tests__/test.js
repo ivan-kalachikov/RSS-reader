72f78ccb80473ae10800a5fc0969c1f0f77348af
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-02-26 15:01:38 +0700

    step 4

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,24 +2,11 @@
 
 import { promises as fs } from 'fs';
 import path from 'path';
-import { getByLabelText } from '@testing-library/dom';
 import rssParser from '../src/rss-parser';
 import expectedParsedData from './__fixtures__/expected';
 
-let rssData;
-
-beforeEach(async () => {
+test('rssParser test', async () => {
   const pathToRss = path.resolve(__dirname, '__fixtures__/rss-feed.xml');
-  rssData = await fs.readFile(pathToRss, 'utf8');
-});
-
-test('rssParser test', () => {
+  const rssData = await fs.readFile(pathToRss, 'utf8');
   expect(rssParser(rssData)).toEqual(expectedParsedData);
-});
-
-test('rss feed url input form test', () => {
-  const container = document.querySelector('main');
-  const urlUnput = getByLabelText(container, 'url');
-  const submitBtn = getByLabelText(container, 'add');
-  console.log(urlUnput, submitBtn);
 });
