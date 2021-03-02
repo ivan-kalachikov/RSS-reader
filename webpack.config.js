1a3908a9c857e4ee3dfe04ca0b2186623bf2327e
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2021-03-02 22:36:18 +0700

    step 5

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
   target: 'web',
   devtool: 'eval-source-map',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
   },
 };
