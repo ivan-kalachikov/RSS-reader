79df366cace167b32e0befe81d1959619376296b
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-02-01 19:03:59 +0800

    minor refactor

---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const proxyURL = 'https://corsproxy.io';
---
+++ src/locales/en.js
@@ -0,0 +1,17 @@
+export default {
+  translation: {
+    feedbackMessages: {
+      alreadyExistRSS: 'RSS already exists',
+      invalidURL: 'The link must be a valid URL',
+      invalidRSS: 'The resource does not contain a valid RSS',
+      networkError: 'Network error',
+      newUrlAdded: 'RSS successfully loaded',
+      unknownError: 'Unknown error {{error.message}}',
+    },
+    ui: {
+      previewButton: 'Preview',
+      feedsTitle: 'Feeds',
+      postsTitle: 'Posts',
+    },
+  },
+};
